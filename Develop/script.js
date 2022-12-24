180ee623f3a0058d63aea13ea6b70cdf3a787c61
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2022-12-24 13:30:36 -0500

    updated schedule time element

--- Develop/script.js
+++ Develop/script.js
@@ -5,3 +5,31 @@ var updateTime = setInterval(function () {
     $("#currentDay").text(currentTime)
 }, 1000);
 
+//Global array variale to store localstorage data
+var schedule=[];
+
+//Audit all schedules
+var auditSchedule = function(rowEl) {
+    // get time from schedule element
+    var scheduletime = $(rowEl).attr("id");
+    var past = moment(scheduletime ,"hha");
+    var t = past.format('LTS');
+    
+    // get current time
+    var prtime = moment().format('LTS');
+    var textEl = $(rowEl).find("textarea");
+
+    var a = moment(t,'hh:mm:ss a');
+    var b = moment(prtime, 'hh:mm:ss a');
+    if(a.isAfter(b,'hours'))
+    {
+        $(textEl).addClass("future"); 
+    }
+    else if(a.isBefore(b,'hours')){
+        $(textEl).addClass("past");
+    }
+    else if(a.isSame(b,'hours')){
+        $(textEl).addClass("present");  
+    }    
+  };
+
