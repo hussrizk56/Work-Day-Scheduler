02d435360d560733b1d271053ca7728f247c1741
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2022-12-25 16:22:58 -0500

    updated js with loading schuled from local sotrage, save button, able to edit logic, and setting date and loading schedule

--- Develop/script.js
+++ Develop/script.js
@@ -32,4 +32,76 @@ var auditSchedule = function(rowEl) {
         $(textEl).addClass("present");  
     }    
   };
+ 
+  //Load all schedules from localstorage
+var loadSchedule = function(){
+  var getSchedule = localStorage.getItem("schedule");
+  
+  if (getSchedule!== null)
+  {
+      getSchedule = JSON.parse(getSchedule);
+      for(var i=0;i<getSchedule.length;i++)
+      {
+          schedule[i]=getSchedule[i];
+          var splitstr=schedule[i].split("-");
+          $('#'+splitstr[0]).find("textarea").text(splitstr[1]);
+      }
+      
+  }
+  $(".row").each(function(index, el) {
+      auditSchedule(el);
+  });
+};
 
+//Get the text and parent id on click of Save button
+$(".row").on("click", "button", function() {
+  var flag = false;
+  var scheduleNew = [];
+  var activityText = $(this).prev().val().trim();
+ 
+  var parentrowId = $(this).closest(".row").attr("id");
+  //Edit schedule logic
+  var getSchedule = localStorage.getItem("schedule");
+  
+  if (getSchedule!== null)
+  {
+      getSchedule = JSON.parse(getSchedule);
+      for(var i=0;i<getSchedule.length;i++)
+      {
+          scheduleNew[i]=getSchedule[i];
+          var splitstr=scheduleNew[i].split("-");
+          if(splitstr[0] === parentrowId)
+          {
+              schedule.splice(i,1);
+              flag = true;
+              break;
+          }
+      }  
+      if(flag===true)
+      {
+          localStorage.clear();
+          localStorage.setItem("schedule",JSON.stringify(schedule));
+      }      
+        
+  }
+
+  var localstr = parentrowId+"-"+activityText;
+  schedule.push(localstr);
+  localStorage.setItem("schedule",JSON.stringify(schedule));   
+   
+});
+
+//Set current date using momentjs
+var todayDate = moment().format("dddd,  MMMM Do");
+$("#currentDay").text(todayDate);
+
+//load schedule on page load
+loadSchedule();
+
+//Audit schedule in every 15 mins
+setInterval(function () {
+  $(".row").each(function(index, el) {
+    auditSchedule(el);
+    window.location.reload();
+  });
+}, 900000);
